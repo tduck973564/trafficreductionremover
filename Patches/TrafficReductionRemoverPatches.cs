11e3cbea07dec06f1bf24e7ba8915117a68a2369
tduck973564 <tduck973564@gmail.com>
2024-02-04 00:11:10 +1100

    initial

---
+++ Patches/TrafficReductionRemoverPatches.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Colossal.Logging;
+using HarmonyLib;
+using Game.Prefabs;
+using Unity.Entities;
+
+namespace TrafficReductionRemover.Patches 
+{
+    [HarmonyPatch(typeof(PrefabSystem), "AddPrefab")]
+    class PrefabSystem_AddPrefabPatch
+    {
+        static void Prefix(PrefabSystem __instance, PrefabBase prefab)
+        {
+            if (prefab is EconomyPrefab)
+            {
+                FieldInfo trafficReduction = AccessTools.Field(typeof(Game.Prefabs.EconomyPrefab), "m_TrafficReduction");
+                trafficReduction.SetValue(prefab, 0f);
+                Console.WriteLine("Added m_TrafficReduction value");
+            }
+        }
+    }
+
+    // This shouldn't be necessary, and probably isn't a good idea, but in case it is required I am leaving it
+    [HarmonyPatch(typeof(PrefabSystem), "UpdatePrefabs")]
+    class PrefabSystem_UpdatePrefabPatch
+    {
+        static void Prefix(PrefabSystem __instance)
+        {
+            Dictionary<PrefabBase, Entity> updateMap = Traverse.Create(__instance).Field("m_UpdateMap")
+                .GetValue<Dictionary<PrefabBase, Entity>>();
+            
+            foreach (KeyValuePair<PrefabBase, Entity> item in updateMap)
+            {
+                if (item.Key is EconomyPrefab)
+                {
+                    FieldInfo trafficReduction =
+                        AccessTools.Field(typeof(Game.Prefabs.EconomyPrefab), "m_TrafficReduction");
+                    
+                    trafficReduction.SetValue(item.Key, 0f);
+                    Traverse.Create(__instance).Field("m_UpdateMap").SetValue(updateMap);
+                    Console.WriteLine("Updated m_TrafficReduction value");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+﻿# TrafficReductionRemover
+
+Disables the restriction that reduces how much traffic spawns the bigger the city gets. This may not work if the game is not relaunched between loading different savefiles.
+
+Thanks to algernon for the help
+
+https://forum.paradoxplaza.com/forum/threads/unlock-developer-restrictions-on-traffic-flow.1608201/
